299d903a7a61f41940a7835d2fbe05ea841b9060
Todd.Whitehead <warriorfullarmorofgod@gmail.com>
2023-10-25 09:18:05 -0700

    First Commit

---
+++ proof-api/src/test/java/com/mod/todd/ModuleProofApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mod.todd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModuleProofApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
